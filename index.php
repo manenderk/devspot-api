f6321c43778e15ffe4538ce1565761a67968f5e0
ManenderK <manender369@gmail.com>
2019-04-13 11:41:18 -0400

    completed minifier api

--- index.php
+++ index.php
@@ -10,19 +10,22 @@ $app = new \Slim\App;
 $app->post('/minify/css', function (Request $request, Response $response) {
 	
 	$responseData=[
-		'status'			=>	'error',
-		'minified_resource'	=> 	'',
-		'message'			=> 	''
-	];
+    'status'            => 'error',
+    'minified_resource' => '',
+    'message'           => '',
+    'original_size'     => 0,
+    'minified_size'     => 0
+  ];
 
 	try{
-		$data = $request->getParsedBody();
-    	$style = filter_var($data['unminified_resource'], FILTER_SANITIZE_STRING);
+		  $data = $request->getParsedBody();
+    	$style = $data['unminified_resource'];
     	$minifier = new Minify\CSS($style);
     	$minified = $minifier->minify();
     	$responseData['status'] = 'success';
-    	$responseData['minified_resource'] = $minified;
-    	unset($minifier);    	
+    	$responseData['minified_resource'] = $minified;  
+      $responseData['original_size']= mb_strlen($style, '8bit');
+      $responseData['minified_size']= mb_strlen($responseData['minified_resource'], '8bit');	
 	}
    	catch(Exception $e){
    		$responseData['status'] = 'error';
@@ -39,30 +42,30 @@ $app->post('/minify/css', function (Request $request, Response $response) {
 $app->post('/minify/js', function (Request $request, Response $response) {
 	
 	$responseData=[
-		'status'			=>	'error',
-		'minified_resource'	=> 	'',
-		'message'			=> 	''
+		'status'            => 'error',
+		'minified_resource' => '',
+		'message'           => '',
+    'original_size'     => 0,
+    'minified_size'     => 0
 	];
 
 	try{
-		$data = $request->getParsedBody();
-    	$script = filter_var($data['unminified_resource'], FILTER_SANITIZE_STRING);
+		  $data = $request->getParsedBody();
+    	$script = $data['unminified_resource'];
     	$minifier = new Minify\JS($script);
     	$minified = $minifier->minify();
     	$responseData['status'] = 'success';
-    	$responseData['minified_resource'] = $minified;
-    	unset($minifier);    	
+    	$responseData['minified_resource'] = $minified; 	
+      $responseData['original_size']= mb_strlen($script, '8bit');
+      $responseData['minified_size']= mb_strlen($responseData['minified_resource'], '8bit');
 	}
-   	catch(Exception $e){
+ 	catch(Exception $e){
    		$responseData['status'] = 'error';
    		$responseData['message'] = $e->getMessage(); 
-   	}
-
-    
-    $response->withHeader('Content-Type', 'application/json');
-    $response->write(json_encode($responseData));
-    return $response;
+ 	}  
+  $response->withHeader('Content-Type', 'application/json');
+  $response->write(json_encode($responseData));
+  return $response;
 });
-
 
 $app->run();
